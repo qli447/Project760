981e24b9858ecba7c6545e268d3c427929f6c871
GB760 <qli447@wisc.edu>
2021-11-29 23:40:39 -0600

    parta

--- schema_postgre.sql
+++ schema_postgre.sql
@@ -1,9 +1,8 @@
 CREATE TABLE PHRASES(
+timestamp DATE NOT NULL,
 tweet_id NUMERIC NOT NULL,
-time_insec DATE NOT NULL,
 text varchar(1000) NOT NULL,
 CONSTRAINT tweet_pkey PRIMARY KEY (tweet_id)
 );
-
 
 
