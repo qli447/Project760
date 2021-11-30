c9f54c5c0fca869f4ee28855292175e59b14f814
GB760 <qli447@wisc.edu>
2021-11-29 20:47:54 -0600

    partb

---
+++ server_postgres.py
@@ -0,0 +1,151 @@
+import psycopg2
+def connect_db():
+    try:
+        conn = psycopg2.connect(database='twitter', user='postgres',
+                                password='123', host='127.0.0.1', port=5432)
+    except Exception as e:
+        # error_logger.error(e)
+        print("fail")
+    else:
+        return conn
+    return None
+def close_db_connection(conn):
+    conn.commit()
+    conn.close()
+connect_db()
+print("success connect to database")
+
+def execute_sql(sql):
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute(sql)
+    close_db_connection(conn)
+    print(sql)
+    
+import requests
+import os
+import json
+import time
+import re
+from datetime import datetime
+
+
+os.environ['BEARER_TOKEN'] = 'AAAAAAAAAAAAAAAAAAAAAGakUQEAAAAAucI%2FuyC5RvtNok3cESZ93ITkRxQ%3DegMxLb1Q1A67ZK5AJJcghTtmyOryBWouBG6TSpLAjDWRSDI3CQ'
+#print(os.environ)
+# To set your environment variables in your terminal run the following line:
+#'BEARER_TOKEN'= AAAAAAAAAAAAAAAAAAAAAGakUQEAAAAAucI%2FuyC5RvtNok3cESZ93ITkRxQ%3DegMxLb1Q1A67ZK5AJJcghTtmyOryBWouBG6TSpLAjDWRSDI3CQ
+bearer_token = os.environ.get("BEARER_TOKEN")
+
+
+def create_url():
+    #return "https://api.twitter.com/2/tweets/sample/stream"
+    return "https://api.twitter.com/2/tweets/sample/stream?tweet.fields=created_at,lang"
+    #add lang to make a filter that we could select words from English
+
+import spacy
+import re
+
+nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
+
+regex = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+
+def clean_text(text):
+   if type(text) != str:
+       text = text.decode("utf-8")
+   doc = re.sub(regex, '', text, flags=re.MULTILINE) # remove URLs
+   sentences = []
+   for sentence in doc.split("\n"):
+       if len(sentence) == 0:
+           continue
+       sentences.append(sentence)
+   doc = nlp("\n".join(sentences))
+   doc = " ".join([token.lemma_.lower().strip() for token in doc
+                       if (not token.is_stop)
+                           and (not token.like_url)
+                           and (not token.lemma_ == "-PRON-")
+                           and (not len(token) < 4)])
+   return doc
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2SampledStreamPython"
+    return r
+
+
+
+def connect_to_endpoint(url):
+    response = requests.request("GET", url, auth=bearer_oauth, stream=True)
+    print(response.status_code)
+    temp = []
+    final = []
+
+    for response_line in response.iter_lines():
+        if response_line:
+            json_response = json.loads(response_line)
+            #print(json.dumps(json_response, indent=4, sort_keys=True))
+            if json_response['data']['lang']=='en':
+                json_data = json_response['data']
+                json_temp = ({k: v for k, v in json_data.items() if k in ('created_at', 'text')})
+
+                temp = json_temp.values()
+
+                keys_values = json_temp.items()
+                temp = {str(key): str(value) for key, value in keys_values}
+
+                temp['text'] = ''.join(filter(lambda character:ord(character) < 0x100,temp['text']))
+                temp['text']=clean_text(temp['text'])
+                final += temp.values()
+                #time.sleep(1)
+
+                print(len(final))
+
+                #myfile = open('text.txt', 'a')
+                # myFile.write(str(final))
+                # myFile.write('\n')  # adds a line between tweets
+                # myFile.close()
+            if len(final) > 1000:
+                timestamp = final[::2]
+                timestamp = [datetime.strptime(i,"%Y-%m-%dT%H:%M:%S.%fZ") for i in timestamp]
+                timestamp = [i.strftime("%Y-%m-%d %H:%M:%S") for i in timestamp]
+
+                text = final[1::2]
+                text = [" ".join(i.split()) for i in text]
+
+                for i in range(round(len(final)/2)):
+                    timestamp_value = "'"+str(timestamp[i])+"'"
+                    text_value = str(text[i])
+                    text_value = text_value.replace('\'', '')
+                    text_value = "'"+text_value+"'"
+                    sql = ("insert into tweet(timestamp,text) "+"values(%s, %s)" % (timestamp_value,text_value)+";")
+                    execute_sql(sql)
+
+                print("done")
+                #time.sleep(60)
+                break
+
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+
+            )
+        )
+    return final
+
+
+def main():
+    url = create_url()
+    timeout = 0
+
+    while True:
+        connect_to_endpoint(url)
+        timeout += 1
+
+if __name__ == "__main__":
+    main()
+
